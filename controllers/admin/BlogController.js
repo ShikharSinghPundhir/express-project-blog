10eaf771f4d9887f27623155b9e1ef3bae684e7d
ShikharSinghPundhir <shikharpundhir84@gmail.com>
2022-12-17 21:04:28 +0530

    mm

--- controllers/admin/BlogController.js
+++ controllers/admin/BlogController.js
@@ -1,6 +1,13 @@
+const BlogModel=require('../../models/Blog')
+
 class BlogController{
-    static blogdisplay=(req,res)=>{
+    static blogdisplay=async(req,res)=>{
+        const data=await BlogModel.find()
+        console.log(data)
         res.render('admin/blog/blogdisplay')
     }
+    
 }
 module.exports=BlogController
+
+
--- controllers/admin/CategoryController.js
+++ controllers/admin/CategoryController.js
@@ -1,6 +1,11 @@
+const CategoryModel=require('../../models/category')
+
 class CategoryController{
-    static categorydisplay=(req,res)=>{
+    static categorydisplay=async(req,res)=>{
+        const data=await CategoryModel.find()
+        console.log(data)
         res.render('admin/category/categorydisplay')
     }
+    
 }
 module.exports=CategoryController
